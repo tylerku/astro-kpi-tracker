909754ebb00bc8c52d64693a32a8b87042251dd3
Ty Udy <ty.udy22@gmail.com>
2023-08-09 00:06:01 -0600

    updated cron job test

---
+++ src/pages/api/sms/test.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+ 
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  response.status(200).json({
+    body: "this is a sample body",
+    query: request.query,
+    cookies: request.cookies,
+  });
+}
