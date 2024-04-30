94a160a071b29d2d7e79908a4bd50661c2281b76
Ty Udy <ty.udy22@gmail.com>
2024-04-30 11:30:38 -0600

    update increment webhook to only update notion data

--- src/pages/api/webhooks/increment.ts
+++ src/pages/api/webhooks/increment.ts
@@ -8,19 +8,20 @@ export default async function GET(
   request: NextApiRequest,
   response: NextApiResponse,
 ){
-    // 1. Get the user tied to the passed in ID
-    console.log(
-      'body: ', request.body
-    )
     if (!request.body.customData.astro_user_id) {
       return response.status(400).json({
         errorMessage: 'No astro_user_id provided',
       }); 
     }
+    if (!request.body.customData.kpi_name) {
+      return response.status(400).json({
+        errorMessage: 'No kpi_name provided',
+      });
+    }
 
+    /*
+    // 1. Get the user tied to the passed in ID
     const user = await userService.getUserById(request.body.customData.astro_user_id)
-    
-    // 2. Get all KPI definitions for the user
     const userId = user && !isNaN(Number(user.id)) ? Number(user!.id) : undefined
     if (!userId) {
       return response.status(400).json({
@@ -28,33 +29,28 @@ export default async function GET(
       }); 
     }
 
-    const kpis = await kpiService.getKPIsForUser(userId)
-
-    // find the kpi that matches the request.body.customData.kpi_name
-    if (!request.body.customData.kpi_name) {
-      return response.status(400).json({
-        errorMessage: 'No kpi_name provided',
-      });
-    }
-    const kpi = kpis.find(kpi => kpi.name === request.body.customData.kpi_name)
+    // 2. Get the KPI to updated 
+    const kpiDefinitions = await kpiService.getKPIsForUser(userId)
+    const kpi = kpiDefinitions.find(kpi => kpi.name === request.body.customData.kpi_name)
     if (!kpi) {
       return response.status(400).json({
         errorMessage: 'No kpi found for the provided kpi_name',
       });
     }
-
     const todaysEntries = await kpiService.getTodaysKPIs(userId, TIMEZONE.MST)
     const kpiToIncrement = todaysEntries.find(entry => entry.definitionId === kpi.definitionId)
-
-    // 3. Increment the KPI
     if (!kpiToIncrement) {
       return response.status(400).json({
         errorMessage: 'No kpi found for the provided kpi_name',
       });
     }
 
+    // 3. Increment the KPI
+    const incrementedKPI = await kpiService.incrementKPI({...kpiToIncrement})
+    */
+
     // 4. Increment the KPI in Notion
-    const useNotion = true // turn this off later if I want
+    const useNotion = true
     if (useNotion) {
       try {
         await incrementNotionKpi(request.body.customData.kpi_name)
@@ -64,11 +60,10 @@ export default async function GET(
         });
       }
     }
-    const incrementedKPI = await kpiService.incrementKPI({...kpiToIncrement})
+    
 
     return response.status(200).json({
-        message: 'test ran successfuly',
-        kpi: { ...incrementedKPI }
+        message: 'ran successfuly',
     });
 }
 
